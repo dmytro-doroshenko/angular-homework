d22519deebdb9f9251e337acaf407fd002b93750
dmytro-doroshenko <57708803+dmytro-doroshenko@users.noreply.github.com>
2020-04-07 19:53:52 +0300

    first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {PostModel} from '../models/PostModel';
+import {PostService} from './services/post.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lesson1-homework';
+  posts: PostModel[];
+
+
+  constructor(private postService: PostService) {
+    this.postService.getPosts().subscribe(posts => this.posts = posts);
+  }
 }
