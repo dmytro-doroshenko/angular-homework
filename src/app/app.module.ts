239146d76cd231c26e25ce180b42609d7f9114aa
dmytro-doroshenko <57708803+dmytro-doroshenko@users.noreply.github.com>
2020-04-09 20:20:49 +0300

    homework 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '../components/app/app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {PostComponent} from '../post/post.component';
-import { CommentComponent } from '../comment/comment.component';
-import { UserComponent } from '../user/user.component';
+import {PostComponent} from '../components/post/post.component';
+import { CommentComponent } from '../components/comment/comment.component';
+import { UserComponent } from '../components/user/user.component';
 import {RouterModule, Routes} from '@angular/router';
-import {UserResolverService} from '../../app/services/user-resolver.service';
-import {PostResolverService} from '../../app/services/post-resolver.service';
-import {CommentResolverService} from '../../app/services/comment-resolver.service';
-import { AllUsersComponent } from '../all-users/all-users.component';
-import { AllPostsComponent } from '../all-posts/all-posts.component';
-import { AllCommentsComponent } from '../all-comments/all-comments.component';
-import { MainComponent } from '../main/main.component';
+import {UserResolverService} from './services/user-resolver.service';
+import {PostResolverService} from './services/post-resolver.service';
+import {CommentResolverService} from './services/comment-resolver.service';
+import { AllUsersComponent } from '../components/all-users/all-users.component';
+import { AllPostsComponent } from '../components/all-posts/all-posts.component';
+import { AllCommentsComponent } from '../components/all-comments/all-comments.component';
+import { MainComponent } from '../components/main/main.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
